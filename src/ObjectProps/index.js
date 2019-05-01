5feb79ac4d6c75a24806abfa7b0165a3069880ff
Michał Chęciński <michal.checinski@outlook.com>
2019-05-01 19:04:34 +0200

    Add object props pane with mock text

---
+++ src/ObjectProps/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ObjectProps.css';
+
+class ObjectProps extends React.Component {
+    render() {
+      return <div className="Object-Props Pane">
+        <h1>This is object properties pane</h1>
+      </div>
+    }
+  }
+
+  export default ObjectProps;
