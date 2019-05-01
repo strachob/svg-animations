22b503a4075f0427d31e49834ff3b923ee2800a0
Michał Chęciński <michal.checinski@outlook.com>
2019-05-01 19:22:59 +0200

    Add ctor and map props to state in objects pane component

--- src/ObjectsPane/index.js
+++ src/ObjectsPane/index.js
@@ -3,6 +3,12 @@ import ObjectList from "./ObjectList";
 import './ObjectPane.css';
 
 class ObjectsPane extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { objects: props.objects };
+  }
+
     render() {
       return <div className="Object-Pane Pane">
         <h1>This is components pane</h1>
