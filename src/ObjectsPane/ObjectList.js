3dbc381346da04fa512425d6dd1058d1d71c46f1
Michał Chęciński <michal.checinski@outlook.com>
2019-05-01 19:21:10 +0200

    Add props to state in object list component

--- src/ObjectsPane/ObjectList.js
+++ src/ObjectsPane/ObjectList.js
@@ -3,6 +3,7 @@ import React from 'react';
 class ObjectList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { objects: props.objects };
     }
 
     render() {
