88329d39d7ca2d6297fbeda44fdf682a955a5527
Bartosz Strachowski <bstrachowski@gmail.com>
2019-05-04 19:10:52 +0200

    ObjectProps Pane showing which figure is being modified

--- src/ObjectProps/index.js
+++ src/ObjectProps/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './ObjectProps.css';
 
 class ObjectProps extends React.Component {
+  // constructor(props){
+  //   super(props);
+  // }
+
     render() {
       return <div className="Object-Props Pane">
         <h1>This is object properties pane</h1>
+        <h2>You are currently modifing {this.props.selectedItem.name}</h2>
       </div>
     }
   }
