71282875a53aabdcfb792024cb6928da1d4cc6c7
Bartosz Strachowski <bstrachowski@gmail.com>
2019-05-04 19:03:44 +0200

    Changed App to be a class

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import ObjectsPane from './ObjectsPane';
 import ObjectProps from './ObjectProps';
 import AnimationPane from './AnimationPane';
 
-function App() {
-  return (
-    <div className="App">
-    <ObjectsPane objects={objects}/>
-    <ObjectProps/>
-    <AnimationPane image={logo}/>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedObjectItem: 0
+    };
+
+    this.selectItemFunc = this.selectItemFunc.bind(this);
+  }
+
+  selectItemFunc(obj){
+    this.setState({selectedObjectItem: obj});
+    console.log(objects[this.state.selectedObjectItem]);
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <ObjectsPane objects={objects} selectItem={this.selectItemFunc}/>
+      <ObjectProps selectedItem={objects[this.state.selectedObjectItem]} />
+      <AnimationPane image={logo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
