f6596e23dbe0944c42eee877bb3098b1f4a26e67
Bartosz Strachowski <bstrachowski@gmail.com>
2019-05-07 12:45:32 +0200

    Adding slider which changes x position

--- src/ObjectProps/index.js
+++ src/ObjectProps/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './ObjectProps.css';
 
 class ObjectProps extends React.Component {
-  // constructor(props){
-  //   super(props);
+  constructor(props){
+    super(props);
+
+
+  }
+
+  // handleChange (event){
+  //  // console.log(event.target.value);
+  //   console.log(this.props.selectedItem.name);
+  //   this.props.selectedItem.name = "dwadawd";
   // }
 
     render() {
       return <div className="Object-Props Pane">
         <h1>This is object properties pane</h1>
         <h2>You are currently modifing {this.props.selectedItem.name}</h2>
+        <p>X position:</p>
+        <input type="range" className="slider" onChange={this.props.posChange.bind(this)} value={this.props.selectedItem.x}/>
+        <p>Current {this.props.selectedItem.name} X is set to: {this.props.selectedItem.x}</p>
       </div>
     }
   }
