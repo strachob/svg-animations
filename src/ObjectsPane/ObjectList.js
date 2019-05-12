d10b432ff2d3ab3c2d1d83ad90346bddd84a2196
Bartosz Strachowski <bstrachowski@gmail.com>
2019-05-12 23:18:58 +0200

    Add scrollable components and todo deleting

--- src/ObjectsPane/ObjectList.js
+++ src/ObjectsPane/ObjectList.js
@@ -3,26 +3,24 @@ import React from 'react';
 class ObjectList extends React.Component {
     constructor(props) {
         super(props);
-        
-        
-        this.state = { objects: props.objects };
-    }
 
-    handleClick(index){
-        this.props.selectItem(index);
+        this.state = { objects: props.objects };
     }
 
     render() {
         const objectsLis = this.state.objects.map((item, index) => {
             return (
-                <li key={index} onClick={() => this.handleClick(index)} className="list-group-item list-group-item-action"> 
+                <li key={index} onClick={(e) => this.props.selectItem(e, index)} className="list-group-item list-group-item-action"> 
                     {item.name}
+                    <button className="btn btn-primary trash-btn" onClick={(e) => this.props.deleteItem(e,index)}>
+                        <i class="fa fa-trash"></i>
+                    </button>
                  </li>
            )
        });
 
         return <div>
-            <ul className="list-group">
+            <ul className="list-group scrollable-config">
                 {objectsLis}
             </ul>
             </div>;
--- src/ObjectsPane/index.js
+++ src/ObjectsPane/index.js
@@ -10,41 +10,50 @@ class ObjectsPane extends React.Component {
       objects: props.objects,
       selected: undefined 
     };
-
-    this.trasnportObjectToParent = this.trasnportObjectToParent.bind(this);
-    this.onClickAdd = this.onClickAdd.bind(this);
   }
 
-  onClickAdd(event) {
+  deleteItemFunc = (e,obj) => {
+ 
+    let objects = this.state.objects
+    let newSelectedIndex = obj;
+    if(obj === this.state.objects.length){
+      newSelectedIndex = obj-2;
+    }
+    this.props.selectItem(e,newSelectedIndex);
+    this.state.objects.splice(obj, 1);
+
+    this.setState({objects});
+  }
+
+  onClickAdd = (event) => {
     let newObjects = this.state.objects;
     let num = newObjects.length+1;
     newObjects.push({name: "Figure "+ num});
     this.setState({ objects : newObjects});
-    this.props.selectItem(newObjects.length-1);
+    this.props.selectItem(event, newObjects.length-1);
     event.preventDefault();
   }
 
-  trasnportObjectToParent(obj){
-    this.props.selectItem(obj);
-  }
-
-    render() {
-      return( 
-      <div className="object-pane pane">
-        <div className="object-pane-header">
-          <h4 className="display-4 figure-header">Select or Add a figure</h4>
-          <button onClick={this.onClickAdd} 
-                  className="btn btn-primary new-obj-btn" 
-                  type="button">
-                   <i className="fa fa-plus"></i>
-              Add new figure
-             
-          </button>
-        </div>
-        <ObjectList objects={this.state.objects} selectItem={this.trasnportObjectToParent} />
+  render() {
+    return( 
+    <div className="object-pane pane">
+      <div className="object-pane-header">
+        <h4 className="display-4 figure-header">Select or Add a figure</h4>
+        <button onClick={this.onClickAdd} 
+                className="btn btn-primary new-obj-btn" 
+                type="button">
+                  <i className="fa fa-plus"></i>
+            Add new figure
+            
+        </button>
       </div>
-      );
-    }
+      <ObjectList 
+          objects={this.state.objects} 
+          selectItem={this.props.selectItem} 
+          deleteItem={this.deleteItemFunc} />
+    </div>
+    );
+  }
   }
 
 
