5d3cdfe1436dcb7d37f2ba84879b30a522ff6c32
Bartosz Strachowski <bstrachowski@gmail.com>
2019-05-23 16:55:48 +0200

    Modals with styles completed

--- src/AnimationPane/index.js
+++ src/AnimationPane/index.js
@@ -70,7 +70,7 @@ class AnimationPane extends React.Component {
   render() {
     return <div className="animation-pane pane">
       <div>
-        <h1 className="display-4 figure-header">See what happends
+        <h1 className="display-4 figure-header">See what happens
         <button className="btn btn-primary export" onClick={() => this.generateExportSvg() }> Export SVG </button>
         <button className="btn btn-primary export" onClick={() => this.openImportModal() }> Import SVG </button></h1>
       </div>
@@ -79,22 +79,25 @@ class AnimationPane extends React.Component {
       <ReactModal
           isOpen={this.state.openExportModal}
           contentLabel="Export Modal"
-        style={this.modalStyles}
+          style={this.modalStyles}
       >
-        <textarea rows="20" cols="100" name="svg" readOnly="true">{this.state.svg}</textarea>
-        <button onClick={() => this.closeExportModal()}>Close</button>
+        <div>
+          <h3>Copy your SVG markup:</h3>
+        <textarea className="form-control" rows="20" cols="100" name="svg" readOnly="true">{this.state.svg}</textarea><br/>
+        <button className="btn btn-info float-right" onClick={() => this.closeExportModal()}>Close</button>
+        </div>
       </ReactModal>
 
       <ReactModal
           isOpen={this.state.openImportModal}
           contentLabel="Import Modal"
-        style={this.modalStyles}
+          style={this.modalStyles}
       >
         <div>
-          SVG to import:<br/>
-          <textarea rows="20" cols="100" name="svg" onChange={(e) => this.setImportSvg(e)}></textarea><br/>
-          <button onClick={() => this.closeImportModalWithImport()}>Import</button>
-          <button onClick={() => this.closeImportModal()}>Close</button>
+          <h3>Paste SVG markup to import your project:</h3>
+          <textarea className="form-control" rows="20" cols="100" name="svg" onChange={(e) => this.setImportSvg(e)}></textarea><br/>
+          <button className="btn btn-primary" onClick={() => this.closeImportModalWithImport()}>Import</button>
+          <button className="btn btn-info float-right" onClick={() => this.closeImportModal()}>Close</button>
         </div>
       </ReactModal>
     </div>
